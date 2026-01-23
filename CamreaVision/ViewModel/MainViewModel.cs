0a61b5242e4742b51db11a19023dfd5d768925e1
lizhenyu <2945659544@qq.com>
2026-01-23 15:30:10 +0800

    初始化相机

--- CamreaVision/ViewModel/MainViewModel.cs
+++ CamreaVision/ViewModel/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.ObjectModel;
 using CamreaVision.Models;
 using CamreaVision.Service;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using LyuWpfHelper.ViewModels;
 using Microsoft.Extensions.Logging;
 using ZLogger;
@@ -12,6 +14,10 @@ public partial class MainViewModel : ViewModelBase
     private readonly ILogger<MainViewModel> _logger;
     private readonly ICameraService _cameraService;
 
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ScanCameraCommand))]
+    public partial bool IsInitialSDK { get; set; }
+
     public ObservableCollection<CameraInfo> Cameras { get; } = [];
 
     public MainViewModel(ICameraService cameraService, ILogger<MainViewModel> logger)
@@ -19,17 +25,18 @@ public partial class MainViewModel : ViewModelBase
         _cameraService = cameraService;
         _logger = logger;
 
-        InitialCamera();       
+        IsInitialSDK = _cameraService.InitializeSdk();    
     }
 
-    private void InitialCamera()
-    {
-        if (_cameraService.InitializeSdk())
-        {
-            _logger.ZLogError($"初始化相机失败！");
-            return;
-        }
 
+    #region MindVision
+
+    [RelayCommand(CanExecute = (nameof(IsInitialSDK)))]
+    private void ScanCamera()
+    {
         _cameraService.EnumerateDevices().ForEach(c => Cameras.Add(c));
     }
+
+
+    #endregion
 }
